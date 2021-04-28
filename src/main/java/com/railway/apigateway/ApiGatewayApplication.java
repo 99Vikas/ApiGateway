98d637d5e6f8cc4467abf14530ac9a50b003252b
Vikas <vschauhan0110@gmail.com>
2021-04-29 01:43:16 +0530

    Added CORS Configurer

--- src/main/java/com/railway/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/railway/apigateway/ApiGatewayApplication.java
@@ -4,11 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println(registry.toString());
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000","http://localhost:3001");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
--- src/main/java/com/railway/apigateway/Security/SecurityTokenConfiguration.java
+++ src/main/java/com/railway/apigateway/Security/SecurityTokenConfiguration.java
@@ -20,6 +20,8 @@ public class SecurityTokenConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
